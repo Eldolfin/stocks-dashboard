f0f322cca2571cec61a50e336ee3b5f331c3b903
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-04-05 01:26:36 +0200

    feat(frontend/favicon): favicon in navbar too

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import '../app.css';
-	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
 	import {
 		DarkMode,
@@ -16,7 +15,6 @@
 		Drawer,
 		CloseButton
 	} from 'flowbite-svelte';
-	import { CurrencyDollar } from 'svelte-heros-v2';
 	import { sineIn } from 'svelte/easing';
 
 	let transitionParams = {
@@ -54,24 +52,17 @@
 	const toggleDrawer = () => {
 		drawerHidden = false;
 	};
-	$: activeUrl = $page.url.pathname;
-	let spanClass = 'pl-2 self-center text-md text-gray-900 whitespace-nowrap dark:text-white';
 	let divClass = 'w-full ml-auto lg:block lg:w-auto order-1 lg:order-none';
 	let ulClass =
 		'flex flex-col py-3 my-4 lg:flex-row lg:my-0 text-sm font-medium gap-4 dark:lg:bg-transparent lg:bg-white lg:border-0';
-	let navDivClass =
-		'bg-white dark:bg-gray-800 text-gray-500 dark:text-gray-400 border-gray-200 dark:border-gray-700 divide-gray-200 dark:divide-gray-700 flex items-center justify-between w-full mx-auto py-1.5 px-4';
 </script>
 
 <svelte:window bind:innerWidth={width} />
 <header class="mx-auto w-full flex-none bg-white dark:bg-slate-950">
 	<Navbar let:hidden let:toggle>
-		<NavHamburger
-			on:click={toggleDrawer}
-			btnClass="focus:outline-none whitespace-normal rounded-lg focus:ring-2 p-1.5 focus:ring-gray-400 hover:bg-gray-100 dark:hover:bg-gray-600 m-0 mr-3 lg:hidden"
-		/>
+		<NavHamburger on:click={toggleDrawer} />
 		<NavBrand href="/" class="lg:ml-64">
-			<CurrencyDollar color="#11CC11" />
+			<img src="/favicon.png" alt="Dashboard Logo" class="mr-3 h-6 sm:h-14" />
 			<span class="self-center pl-4 text-xl font-semibold whitespace-nowrap dark:text-white">
 				Wallstreet Bets
 			</span>
@@ -83,12 +74,12 @@
 			nonActiveClass="md:!pl-3 md:!py-2 lg:!pl-0 text-gray-700 hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 dark:text-white lg:dark:hover:text-primary-700 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent"
 			activeClass="md:!pl-3 md:!py-2 lg:!pl-0 lg:text-primary-700 text-white dark:text-white dark:lg:text-primary-500 bg-primary-700 lg:bg-transparent dark:bg-primary-600 lg:dark:bg-transparent cursor-default"
 		>
-			<NavLi class="lg:mb-0 lg:px-2" active={activeUrl === '/'} href="/">Home</NavLi>
+			<NavLi class="lg:mb-0 lg:px-2" href="/">Home</NavLi>
 		</NavUl>
 		<div class="ml-auto flex items-center">
 			<DarkMode class="inline-block hover:text-gray-900 dark:hover:text-white" />
 		</div>
-		<NavHamburger on:click={toggle} btnClass="lg:hidden" />
+		<NavHamburger on:click={toggle} />
 	</Navbar>
 </header>
 
@@ -108,7 +99,7 @@
 	<Sidebar asideClass="w-54">
 		<SidebarWrapper divClass="overflow-y-auto py-4 px-3 rounded dark:bg-gray-800">
 			<SidebarGroup>
-				<SidebarItem label="Home" href="/" on:click={toggleSide} active={activeUrl === `/`} />
+				<SidebarItem label="Home" href="/" on:click={toggleSide} />
 			</SidebarGroup>
 		</SidebarWrapper>
 	</Sidebar>
