d9c1f9ac0e55d501921f15535b28045729ed954c
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-04-04 14:37:48 +0200

    feat(frontend): init svelte project

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { Alert } from 'flowbite-svelte';
+</script>
+<h1>Le dashboard finance!! 🔥🔥🔥</h1>
+
+<div class="p-8">
+  <Alert>
+    <span class="font-medium">Info alert!</span>
+    Change a few things up and try submitting again.
+  </Alert>
+</div>
