9f90b37c823df05664569fc094088b1227994065
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-04-04 22:17:24 +0200

    fix(chart): update issue when changing period

--- frontend/src/lib/components/HistoryChart.svelte
+++ frontend/src/lib/components/HistoryChart.svelte
@@ -1,57 +1,57 @@
 <script lang="ts">
 	import Chart, { type ChartConfiguration } from 'chart.js/auto';
-	import { CHART_COLORS, months, transparentize } from '$lib/chart-utils';
+	import { CHART_COLORS, transparentize } from '$lib/chart-utils';
 	import { onDestroy, onMount } from 'svelte';
 	const { title, dataset, yValuesIndex, color } = $props();
-	const data = {
-		labels: [],
-		datasets: [
-			{
-				label: 'Price',
-				data: dataset,
-				borderColor: CHART_COLORS[color],
-				backgroundColor: transparentize(CHART_COLORS[color], 0.5),
-				yAxisID: 'y',
-				parsing: {
-					yAxisKey: yValuesIndex,
-					xAxisKey: 'Date'
-				}
-			}
-		]
-	};
-	const config = {
-		type: 'line',
-		data: data,
-		options: {
-			responsive: true,
-			interaction: {
-				mode: 'index',
-				intersect: false
-			},
-			// stacked: false,
-			plugins: {
-				title: {
-					display: true,
-					text: title
-				}
-			},
-			scales: {
-				y: {
-					type: 'linear',
-					display: true,
-					position: 'left'
-				},
-				x: {
-					type: 'timeseries'
-				}
-			}
-		}
-	} satisfies ChartConfiguration;
-
 	let chartElt;
 	let chartInstance;
 
 	const createChart = () => {
+		const data = {
+			labels: [],
+			datasets: [
+				{
+					label: 'Price',
+					data: dataset,
+					borderColor: CHART_COLORS[color],
+					backgroundColor: transparentize(CHART_COLORS[color], 0.5),
+					yAxisID: 'y',
+					parsing: {
+						yAxisKey: yValuesIndex,
+						xAxisKey: 'Date'
+					}
+				}
+			]
+		};
+		const config = {
+			type: 'line',
+			data: data,
+			options: {
+				responsive: true,
+				interaction: {
+					mode: 'index',
+					intersect: false
+				},
+				// stacked: false,
+				plugins: {
+					title: {
+						display: true,
+						text: title
+					}
+				},
+				scales: {
+					y: {
+						type: 'linear',
+						display: true,
+						position: 'left'
+					},
+					x: {
+						type: 'timeseries'
+					}
+				}
+			}
+		} satisfies ChartConfiguration;
+
 		if (chartInstance) {
 			chartInstance.destroy(); // Destroy old chart before creating a new one
 		}
