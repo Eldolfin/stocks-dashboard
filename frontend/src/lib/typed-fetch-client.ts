49e0d29436e2189e074e67109932166a9cfc5c6d
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-08-13 01:07:53 +0200

    fix(front/prod): change adapter node -> deno

--- frontend/src/lib/typed-fetch-client.ts
+++ frontend/src/lib/typed-fetch-client.ts
@@ -2,7 +2,10 @@ import createClient from 'openapi-fetch';
 import type { paths } from '../generated/api';
 import { browser } from '$app/environment';
 
-export const baseUrl = browser ? '/' : 'http://backend:5000';
+const baseUrlExternal: string = import.meta.env.VITE_API_BASE_EXTERNAL ?? '/';
+const baseUrlInternal: string = import.meta.env.VITE_API_BASE_INTERNAL ?? 'http://backend:5000';
+
+export const baseUrl = browser ? baseUrlExternal : baseUrlInternal;
 
 export const client = createClient<paths>({
 	baseUrl,
