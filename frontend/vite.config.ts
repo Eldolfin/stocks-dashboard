4dd9977958371f62e66af2ba1a943509fb6609e7
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-08-13 01:41:43 +0200

    trying things...

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,12 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
 	server: {
+		host: '0.0.0.0',
 		port: 3000
-	}
+	},
+	build: {
+		target: 'esnext',
+		assetsInlineLimit: 0,
+	},
+	base: '', // forces relative paths
 });
